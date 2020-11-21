af0d65eb115a963b1fed6bfbd29ecf0c0289a89a
jaswindersroya <74692394+jaswindersroya@users.noreply.github.com>
2020-11-21 18:34:05 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+//WAP to check whether the no is +ve or -ve.
+//FORMULA  -ve  
+#include <stdio.h>
+
+int main()
+{
+    //declare variables
+    int num;
+    
+    //user input
+    printf("enter a negitive of postive number");
+    
+    scanf("%d",&num);
+    
+    if(num>0)
+    printf("number %d is postive",num);
+    else
+    printf("number %d is negitive",num);
+    
+    printf("whatever the number is,Life is beautiful!");
+    
+
+}
+
